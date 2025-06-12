c9413f514f9c441f40219fde2109347ee4044498
rhuaannn <rhuanmarques1223@gmail.com>
2025-06-11 22:49:29 -0300

    Feat: Add model transfer

---
+++ finance.domain/Model/Transfer.cs
@@ -0,0 +1,25 @@
+﻿namespace backend.finance.domain.Model
+{
+    public class Transfer
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public double Amount { get; set; }
+        public Account AccountId { get; set; }
+        public User UserId { get; set; }
+        public ICollection<Account> Accounts { get; set; } = new List<Account>();
+        public ICollection<User> Users { get; set; } = new List<User>();
+
+        protected Transfer()
+        {
+            
+        }
+
+        public Transfer(DateTime createdAt, double amount)
+        {
+            CreatedAt = createdAt;
+            Amount = amount;
+
+        }
+    }
+}
